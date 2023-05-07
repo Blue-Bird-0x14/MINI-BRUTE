6177248bb3416037c35bf913074991eab3356cc0
Blue-Bird-0x14 <121484676+Blue-Bird-0x14@users.noreply.github.com>
2023-05-07 21:43:56 +0600

    Create MINI.py

---
+++ MINI.py
@@ -0,0 +1,9 @@
+import os, sys, platform
+bit = platform.architecture()[0]
+if bit == '64bit':
+    os.system('clear')
+    os.system('git pull')
+    from GIGA import void
+    GIGA.void()
+elif bit == '32bit':
+    exit('32 NOT SAPPORTED')
